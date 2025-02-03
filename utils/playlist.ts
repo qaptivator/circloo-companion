aaa43adf368d48c0ed2e2ae72fd3afd5b0187724
captivator <84224501+qaptivator@users.noreply.github.com>
2025-02-03 14:14:36 +0200

    added queryToPlaylist

--- utils/playlist.ts
+++ utils/playlist.ts
@@ -8,6 +8,12 @@ export type Playlist = {
 }
 
 export function playlistToQuery(playlist: Playlist): string {
-	return JSON.stringify(playlist)
+	//return JSON.stringify(playlist)
 	//return encodeURIComponent(JSON.stringify(playlist))
+	// raw string is big, base64 is bigger, base65536 is MUCH bigger, LZString is compact
+	return LZString.compressToEncodedURIComponent(JSON.stringify(playlist))
+}
+
+export function queryToPlaylist(query: string): Playlist {
+	return JSON.parse(LZString.decompressFromEncodedURIComponent(query))
 }
