76495159dfafaad0a26a7cb1577b8e392b74791a
captivator <84224501+qaptivator@users.noreply.github.com>
2025-02-03 12:13:59 +0200

    added PlaylistsView to tablist

--- pages/browse.vue
+++ pages/browse.vue
@@ -12,11 +12,7 @@
 				}"
 			>
 				<Tab value="0">Search</Tab>
-				<Tab
-					value="1"
-					disabled
-					>Playlists (WIP)</Tab
-				>
+				<Tab value="1">Playlists</Tab>
 				<Tab
 					value="2"
 					disabled
@@ -32,15 +28,7 @@
 					<SearchView />
 				</TabPanel>
 				<TabPanel value="1">
-					<p class="m-0">
-						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-						quae ab illo inventore veritatis et quasi architecto beatae vitae
-						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-						eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci
-						velit, sed quia non numquam eius modi.
-					</p>
+					<PlaylistsView />
 				</TabPanel>
 				<TabPanel value="2">
 					<p class="m-0">
